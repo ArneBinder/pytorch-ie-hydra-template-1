b989d12dcc1d2d4e7da7e7cf6d44fbe2c3d35842
Arne Binder <arne.binder@dfki.de>
2022-07-19 16:18:47 +0200

    remove bridge source code package

--- src/bridge/transformer_token_classification.py
+++
@@ -1,9 +0,0 @@
-from typing import Any, Dict
-
-from pytorch_ie.taskmodules import TransformerTokenClassificationTaskModule
-
-
-def additional_model_kwargs(
-    taskmodule: TransformerTokenClassificationTaskModule,
-) -> Dict[str, Any]:
-    return dict(num_classes=len(taskmodule.label_to_id))
