158ca14b3a2758dd72cddc82c5a3fcad3bd95351
Arne Binder <arne.binder@dfki.de>
2022-07-21 15:13:34 +0200

    fix and improve setup_symlinks.sh

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-## This script creates symlinks to outsource log folders. It requires a target directory as single
-## parameter, e.g:
-# bash setup_symlinks.sh $HOME/experiments/my-project
+## Log and model folders can grow very large. This script creates symlinks to outsource them.
+## It requires a target directory as single parameter, e.g:
+##
+## bash setup_symlinks.sh $HOME/experiments/my-project
+##
+## Note, that training models with a different name than "default" may result in a different
+## save_dir (see config/train.yaml) which needs to be symlinked manually, if outsourcing them
+## is intended.
 
 EXPERIMENTS_DIR=$1
 [[ -z "$EXPERIMENTS_DIR" ]] && { echo "Error: No target directory was provided"; exit 1; }
-DIRS=( "logs/runs" "logs/multiruns" "logs/experiments" "logs/debugs" "logs/evaluations" )
+DIRS=( "logs/experiments" "logs/evaluations" "logs/debugs" "models/default" "models/conll2003" )
 
 echo "symlink to $EXPERIMENTS_DIR..."
 for d in "${DIRS[@]}"
