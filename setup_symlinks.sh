79cb1eb25b9c7efc8451db1116d29c8fab9959cd
Arne Binder <arne.binder@dfki.de>
2022-08-01 14:05:43 +0200

    remove empty model directory to symlink it as a whole

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -4,14 +4,11 @@
 ## It requires a target directory as single parameter, e.g:
 ##
 ## bash setup_symlinks.sh $HOME/experiments/my-project
-##
-## Note, that training models with a different name than "default" may result in a different
-## save_dir (see config/train.yaml) which needs to be symlinked manually, if outsourcing them
-## is intended.
+
 
 TARGET_DIR=$1
 [[ -z "$TARGET_DIR" ]] && { echo "Error: No target directory was provided"; exit 1; }
-DIRS=( "logs/experiments" "logs/evaluations" "logs/debugs" "models/default" "models/conll2003" )
+DIRS=( "logs/experiments" "logs/evaluations" "logs/debugs" "models" )
 
 echo "symlink to $TARGET_DIR..."
 for d in "${DIRS[@]}"
