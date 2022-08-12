b1766b2eea65d75a9a5ae8fd0031d59728efd9f9
Arne Binder <arne.binder@dfki.de>
2022-08-12 17:52:15 +0200

    add "replace" config resolver

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 import dotenv
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 # load environment variables from `.env` file if it exists
 # recursively searches for `.env` in all folders starting from work dir
 dotenv.load_dotenv(override=True)
+
+# register replace resolver (used to replace "/" with "-" in names to use them as wandb project names)
+OmegaConf.register_new_resolver("replace", lambda s, x, y: s.replace(x, y))
 
 
 @hydra.main(config_path="configs/", config_name="train.yaml")
