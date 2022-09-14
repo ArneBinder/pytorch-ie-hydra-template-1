442cbd96f9ab8392f4dbec4b1effafc412d92573
Arne Binder <arne.binder@dfki.de>
2022-09-14 11:57:44 +0200

    remove non-existing import

--- src/taskmodules/__init__.py
+++
@@ -1,7 +0,0 @@
-from taskmodules.simple_transformer_text_classification import (
-    SimpleTransformerTextClassificationTaskModule,
-)
-
-__all__ = [
-    "SimpleTransformerTextClassificationTaskModule",
-]
