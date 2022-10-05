b69b774f6510d72a7832ca82d53a2b63fecdf31a
Arne Binder <arne.binder@dfki.de>
2022-10-05 15:32:22 +0200

    register omegaconf "replace" resolver for evaluate und predict pipelines

--- evaluate.py
+++ evaluate.py
@@ -3,11 +3,14 @@ from os.path import dirname, join, realpath
 
 import dotenv
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 # load environment variables from `.env` file if it exists
 # recursively searches for `.env` in all folders starting from work dir
 dotenv.load_dotenv(override=True)
+
+# register replace resolver (used to replace "/" with "-" in names to use them as wandb project names)
+OmegaConf.register_new_resolver("replace", lambda s, x, y: s.replace(x, y))
 
 
 @hydra.main(version_base="1.2", config_path="configs/", config_name="evaluate.yaml")
