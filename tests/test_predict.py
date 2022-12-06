04e51bce6d638a4763235d8aeefa9195c987e5ae
Arne Binder <arne.binder@dfki.de>
2022-12-06 17:10:18 +0100

    fix prediction tests

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,4 +1,4 @@
-import os
+from os import path
 
 import pytest
 from hydra.core.hydra_config import HydraConfig
@@ -9,31 +9,31 @@ from tests.helpers.run_if import RunIf
 
 
 @pytest.mark.slow
-def test_predict(tmp_path, cfg_predict):
-    """Run inference on cpu."""
+def test_predict_cpu(tmp_path, cfg_predict):
+    """Run inference on CPU."""
 
     HydraConfig().set_config(cfg_predict)
     predict(cfg_predict)
 
-    assert os.path.exists(f"{cfg_predict.prediction_save_dir}/documents.jsonl")
-    assert os.path.exists(f"{cfg_predict.prediction_save_dir}/config.yaml")
+    assert path.exists(path.join(cfg_predict.prediction_save_dir, "documents.jsonl"))
+    assert path.exists(path.join(cfg_predict.prediction_save_dir, "config.yaml"))
 
 
-def test_predict_fast_dev_run(tmp_path, cfg_predict):
-    """Run inference for one batch on cpu."""
+def test_predict_cpu_fast_dev_run(tmp_path, cfg_predict):
+    """Run inference for one batch on CPU."""
     with open_dict(cfg_predict):
         cfg_predict.pipeline.fast_dev_run = True
 
     HydraConfig().set_config(cfg_predict)
     predict(cfg_predict)
 
-    assert os.path.exists(f"{cfg_predict.out_path}.jsonl")
-    assert os.path.exists(f"{cfg_predict.out_path}.config.yaml")
+    assert path.exists(path.join(cfg_predict.prediction_save_dir, "documents.jsonl"))
+    assert path.exists(path.join(cfg_predict.prediction_save_dir, "config.yaml"))
 
 
 @RunIf(min_gpus=1)
 def test_predict_gpu(tmp_path, cfg_predict):
-    """Run for 1 train, val and test step on GPU."""
+    """Run inference on GPU."""
 
     with open_dict(cfg_predict):
         cfg_predict.pipeline.device = 0
@@ -41,5 +41,5 @@ def test_predict_gpu(tmp_path, cfg_predict):
     HydraConfig().set_config(cfg_predict)
     predict(cfg_predict)
 
-    assert os.path.exists(f"{cfg_predict.out_path}.jsonl")
-    assert os.path.exists(f"{cfg_predict.out_path}.config.yaml")
+    assert path.exists(path.join(cfg_predict.prediction_save_dir, "documents.jsonl"))
+    assert path.exists(path.join(cfg_predict.prediction_save_dir, "config.yaml"))
