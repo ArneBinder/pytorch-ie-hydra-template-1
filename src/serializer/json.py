92eb22dacc8c3ec33b5c73136254c3e718ef387c
Arne Binder <arne.binder@dfki.de>
2023-06-15 18:35:52 +0200

    add src.serializer.interface.DocumentSerializer and update json serializer code and config

--- src/serializer/json.py
+++ src/serializer/json.py
@@ -1,31 +1,60 @@
 import json
 import os
-from typing import Sequence
+from typing import Dict, List, Sequence, Type, TypeVar
 
 from pytorch_ie.core import Document
 
-from src import utils
+from src.serializer.interface import DocumentSerializer
+from src.utils import get_pylogger
 
-log = utils.get_pylogger(__name__)
+log = get_pylogger(__name__)
+
+D = TypeVar("D", bound=Document)
 
 
-class JsonSerializer:
-    def __init__(self, path: str, as_lines: bool = True, **kwargs):
+def as_json_lines(file_name: str) -> bool:
+    if file_name.lower().endswith(".jsonl"):
+        return True
+    elif file_name.lower().endswith(".json"):
+        return False
+    else:
+        raise Exception(f"unknown file extension: {file_name}")
+
+
+class JsonSerializer(DocumentSerializer):
+    def __init__(self, path: str, **kwargs):
         self.path = path
-        self.as_lines = as_lines
         self.kwargs = kwargs
 
-    def __call__(self, documents: Sequence[Document]):
+    def __call__(self, documents: Sequence[Document]) -> Dict[str, str]:
+        return self.dump(documents=documents, path=self.path, **self.kwargs)
 
-        realpath = os.path.realpath(self.path)
+    @classmethod
+    def dump(cls, documents: Sequence[Document], path: str, **kwargs) -> Dict[str, str]:
+        realpath = os.path.realpath(path)
         log.info(f'serialize documents to "{realpath}" ...')
         dir_path = os.path.dirname(realpath)
         os.makedirs(dir_path, exist_ok=True)
-
-        if self.as_lines:
-            with open(self.path, "w") as f:
+        if as_json_lines(str(path)):
+            with open(realpath, "w") as f:
                 for doc in documents:
-                    f.write(json.dumps(doc.asdict(), **self.kwargs) + "\n")
+                    f.write(json.dumps(doc.asdict(), **kwargs) + "\n")
         else:
-            with open(self.path, "w") as f:
-                json.dump([doc.asdict() for doc in documents], fp=f, **self.kwargs)
+            with open(realpath, "w") as f:
+                json.dump([doc.asdict() for doc in documents], fp=f, **kwargs)
+        return {"path": realpath}
+
+    @classmethod
+    def read(cls, file_name: str, document_type: Type[D]) -> List[D]:
+        documents = []
+        if as_json_lines(str(file_name)):
+            with open(file_name) as f:
+                for line in f:
+                    json_dict = json.loads(line)
+                    documents.append(document_type.fromdict(json_dict))
+        else:
+            with open(file_name) as f:
+                json_list = json.load(f)
+            for json_dict in json_list:
+                documents.append(document_type.fromdict(json_dict))
+        return documents
