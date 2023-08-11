c1cc8d6c539ba3faf3e71fde32e28e96b322c2a7
ArneBinder <ArneBinder@users.noreply.github.com>
2023-08-11 18:29:46 +0200

    remove statistic_utils (#116)
    * evaluate_documents entry script: use parameter "splits" instead of "split"
    
    * add required pytorch-ie PR as comment
    
    * remove everything statistics related from configs
    
    * remove src.utils.statistic_utils
    
    * add statistic configs: count_entity_labels, count_text_characters, and count_text_tokens

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,4 @@
 from .config_utils import execute_pipeline, instantiate_dict_entries, prepare_omegaconf
 from .logging_utils import close_loggers, get_pylogger, log_hyperparameters
 from .rich_utils import enforce_tags, print_config_tree
-from .statistic_utils import collect_statistics
 from .task_utils import extras, save_file, task_wrapper
