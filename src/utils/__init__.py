59b5cf29ff89e1e890b7f8a017257bf550c7e281
Arne Binder <arne.binder@dfki.de>
2023-08-30 17:54:52 +0200

    implement replace_sys_args_with_values_from_files() and load_value_from_file()

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
 from .config_utils import execute_pipeline, instantiate_dict_entries, prepare_omegaconf
 from .logging_utils import close_loggers, get_pylogger, log_hyperparameters
 from .rich_utils import enforce_tags, print_config_tree
-from .task_utils import extras, save_file, task_wrapper
+from .task_utils import extras, replace_sys_args_with_values_from_files, save_file, task_wrapper
