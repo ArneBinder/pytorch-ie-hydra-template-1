df0f95013c37374d82b5d01d4e64795eda7deb82
ArneBinder <ArneBinder@users.noreply.github.com>
2023-09-19 16:47:49 +0200

    cleanup documents and annotations (#134)
    * cleanup documents and annotations
    
    * remove conversion.py

--- src/document/types.py
+++ src/document/types.py
@@ -1,74 +1,42 @@
 import dataclasses
 from typing import Optional
 
-from pytorch_ie.annotations import BinaryRelation, LabeledSpan, Span, _post_init_single_label
+from pytorch_ie.annotations import LabeledSpan
 from pytorch_ie.core import Annotation, AnnotationList, annotation_field
-from pytorch_ie.documents import TextBasedDocument, TokenBasedDocument
+from pytorch_ie.documents import TextBasedDocument, TextDocumentWithLabeledEntitiesAndRelations
 
 # =========================== Annotation Types ============================= #
 
 
 @dataclasses.dataclass(eq=True, frozen=True)
 class Attribute(Annotation):
-    target_annotation: Annotation
+    annotation: Annotation
     label: str
-    value: Optional[str] = None
-    score: float = 1.0
+    type: Optional[str] = None
+    score: Optional[float] = dataclasses.field(default=None, compare=False)
 
     def __post_init__(self) -> None:
-        _post_init_single_label(self)
+        if not isinstance(self.label, str):
+            raise ValueError("label must be a single string.")
+        if not (self.score is None or isinstance(self.score, float)):
+            raise ValueError("score must be a single float.")
+
+    def __str__(self) -> str:
+        if self.target is not None:
+            result = f"label={self.label},annotation={self.annotation}"
+        else:
+            result = f"label={self.label}"
+        if self.type is not None:
+            result += f",type={self.type}"
+        if self.score is not None:
+            result += f",score={self.score}"
+        return f"{self.__class__.__name__}({result})"
 
 
 # ============================= Document Types ============================= #
 
 
 @dataclasses.dataclass
-class TokenDocumentWithEntitiesAndRelations(TokenBasedDocument):
-    entities: AnnotationList[Span] = annotation_field(target="tokens")
-    relations: AnnotationList[BinaryRelation] = annotation_field(target="entities")
-
-
-@dataclasses.dataclass
-class TokenDocumentWithLabeledEntitiesAndRelations(TokenBasedDocument):
-    entities: AnnotationList[LabeledSpan] = annotation_field(target="tokens")
-    relations: AnnotationList[BinaryRelation] = annotation_field(target="entities")
-
-
-@dataclasses.dataclass
-class TextDocumentWithEntityMentions(TextBasedDocument):
-    entity_mentions: AnnotationList[LabeledSpan] = annotation_field(target="text")
-
-
-@dataclasses.dataclass
-class TextDocumentWithEntitiesAndRelations(TextBasedDocument):
-    """Possible input class for TransformerRETextClassificationTaskModule."""
-
-    entities: AnnotationList[Span] = annotation_field(target="text")
-    relations: AnnotationList[BinaryRelation] = annotation_field(target="entities")
-
-
-@dataclasses.dataclass
-class TextDocumentWithLabeledEntitiesAndRelations(TextBasedDocument):
-    """Possible input class for TransformerRETextClassificationTaskModule."""
-
+class TextDocumentWithLabeledEntitiesAndEntityAttributes(TextBasedDocument):
     entities: AnnotationList[LabeledSpan] = annotation_field(target="text")
-    relations: AnnotationList[BinaryRelation] = annotation_field(target="entities")
-
-
-@dataclasses.dataclass
-class DocumentWithEntitiesRelationsAndLabeledPartitions(TextBasedDocument):
-    """Possible input class for TransformerRETextClassificationTaskModule."""
-
-    entities: AnnotationList[LabeledSpan] = annotation_field(target="text")
-    relations: AnnotationList[BinaryRelation] = annotation_field(target="entities")
-    partitions: AnnotationList[LabeledSpan] = annotation_field(target="text")
-
-
-@dataclasses.dataclass
-class BratDocument(TextBasedDocument):
-    """Possible input class for TransformerRETextClassificationTaskModule."""
-
-    spans: AnnotationList[LabeledSpan] = annotation_field(target="text")
-    relations: AnnotationList[BinaryRelation] = annotation_field(target="spans")
-    span_attributions: AnnotationList[Attribute] = annotation_field(target="spans")
-    relation_attributions: AnnotationList[Attribute] = annotation_field(target="relations")
+    entity_attributes: AnnotationList[Attribute] = annotation_field(target="entities")
