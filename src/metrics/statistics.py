86e9482d8a339ba9d0a4697b8a68d388550e6b0a
Arne Binder <arne.binder@dfki.de>
2024-02-19 11:13:19 +0100

    use distance from span_utils and collect per label

--- src/metrics/statistics.py
+++ src/metrics/statistics.py
@@ -1,12 +1,14 @@
+from collections import defaultdict
 from typing import Any, Dict, List, Optional, Type, Union
 
-from pie_utils.span.slice import distance
-from pytorch_ie import tokenize_document
+from pie_modules.document.processing import tokenize_document
 from pytorch_ie.annotations import BinaryRelation, NaryRelation, Span
 from pytorch_ie.core import Document, DocumentStatistic
 from pytorch_ie.documents import TextBasedDocument, TokenBasedDocument
 from pytorch_ie.utils.hydra import resolve_target
 from transformers import AutoTokenizer, PreTrainedTokenizer
+
+from src.utils import distance
 
 
 class RelationArgumentDistanceCollector(DocumentStatistic):
@@ -27,11 +29,13 @@ class RelationArgumentDistanceCollector(DocumentStatistic):
         tokenize_kwargs: Optional[Dict[str, Any]] = None,
         tokenizer: Optional[Union[str, PreTrainedTokenizer]] = None,
         tokenized_document_type: Optional[Union[str, Type[TokenBasedDocument]]] = None,
+        key_all: str = "ALL",
         **kwargs,
     ):
         super().__init__(**kwargs)
         self.layer = layer
         self.distance_type = distance_type
+        self.key_all = key_all
         self.tokenize = tokenize
         self.tokenize_kwargs = tokenize_kwargs or {}
         if self.tokenize:
@@ -50,7 +54,7 @@ class RelationArgumentDistanceCollector(DocumentStatistic):
                 tokenized_document_type
             )
 
-    def _collect(self, doc: Document) -> List[int]:
+    def _collect(self, doc: Document) -> Dict[str, List[float]]:
         if self.tokenize:
             if not isinstance(doc, TextBasedDocument):
                 raise ValueError(
@@ -68,15 +72,17 @@ class RelationArgumentDistanceCollector(DocumentStatistic):
             )
         else:
             docs = [doc]
-        values = []
+        values: Dict[str, List[float]] = defaultdict(list)
         for doc in docs:
             layer_obj = getattr(doc, self.layer)
 
             for binary_relation in layer_obj:
                 if isinstance(binary_relation, BinaryRelation):
                     args = [binary_relation.head, binary_relation.tail]
+                    label = binary_relation.label
                 elif isinstance(binary_relation, NaryRelation):
                     args = binary_relation.arguments
+                    label = binary_relation.label
                 else:
                     raise TypeError(
                         f"argument distance calculation is not yet supported for {type(binary_relation)}"
@@ -96,5 +102,17 @@ class RelationArgumentDistanceCollector(DocumentStatistic):
                             distance_type=self.distance_type,
                         )
 
-                        values.append(d)
-        return values
+                        values[label].append(d)
+
+        if self.key_all in values:
+            raise ValueError(
+                f'key key_all="{self.key_all}" is reserved for the aggregation of all values. Please '
+                f"choose another value for key_all which is not used as a label of any annotation."
+            )
+        labels = list(values)
+        for label in labels:
+            values[self.key_all].extend(values[label])
+
+        # improve order of entries for histogram plotting: first, the key_all entry, then the rest,
+        # so that not the key_all entry is plotted last and thus covers all other entries
+        return {label: values[label] for label in [self.key_all] + labels}
