443c090c0738d97621da71b4cb9e3259fdf641b3
Arne Binder <arne.binder@dfki.de>
2025-05-05 13:31:53 +0200

    DocumentSerializer requires split parameter

--- src/serializer/interface.py
+++ src/serializer/interface.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, Iterable
+from typing import Any, Iterable, Optional
 
 from pytorch_ie.core import Document
 
@@ -10,6 +10,9 @@ class DocumentSerializer(ABC):
     The serializer should not return the serialized documents, but write them to disk and instead
     return some metadata such as the path to the serialized documents.
     """
+
+    @abstractmethod
+    def __init__(self, split: Optional[str] = None, **kwargs) -> None: ...
 
     @abstractmethod
     def __call__(self, documents: Iterable[Document], append: bool = False, **kwargs) -> Any: ...
